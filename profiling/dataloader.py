d17ed94ab1df4ee252ade1d1f44c4db857c302a4
Håvard Pedersen Brandal <haavard.brandal@gmail.com>
2025-04-16 00:45:58 +0200

    Found 92% speed increase using generator instead of random access in svo

---
+++ profiling/dataloader.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3a8999282127a3d4480a6b25875b233756064c4a8c2084dc9384e43a3811c6c7
+size 319
---
+++ profiling/load_dataset.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:05b0a376c684c6a39b3a6acdd699fe9d0fe1364556953332f914fa66846c6030
+size 238
