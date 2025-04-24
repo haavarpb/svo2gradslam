05f0309c23f785b55f16c790a3e442880500bf81
Håvard Pedersen Brandal <haavard.brandal@gmail.com>
2025-04-24 11:06:33 +0200

    Pass kwargs from plugin call

--- src/svo2gradslam/plugin.py
+++ src/svo2gradslam/plugin.py
@@ -5,4 +5,5 @@ def svo_dataset(config_dict, basedir, sequence, **kwargs):
     if "svo_file" not in config_dict.keys():
         raise ValueError("SVO dataset requires svo_file")
     svo_file = config_dict["svo_file"]
-    return SVOIterableDataset(svo_file, kwargs=kwargs)
+
+    return SVOIterableDataset(svo_file, **kwargs)
