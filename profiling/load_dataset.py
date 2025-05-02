505e6e0c1d570e14ebf1170ec558d4bf55c78abc
Håvard Pedersen Brandal <haavard.brandal@gmail.com>
2025-05-02 09:40:55 +0200

    Profile using pytorch profiler

--- profiling/load_dataset.py
+++ profiling/load_dataset.py
@@ -1,10 +1,15 @@
-import pyinstrument
 import tqdm
+from torch.profiler import ProfilerActivity, profile
 
 from svo2gradslam.svo_dataset import SVOIterableDataset, sofa_filepath
 
-dataset = SVOIterableDataset(str(sofa_filepath()))
+device = 'cuda'
 
-with pyinstrument.profile():
+activities = [ProfilerActivity.CPU, ProfilerActivity.CUDA, ProfilerActivity.XPU]
+dataset = SVOIterableDataset(str(sofa_filepath()), end=10)
+
+with profile(activities=activities) as prof:
     for sample in tqdm.tqdm(dataset):
         sample
+
+prof.export_chrome_trace("trace.json")
